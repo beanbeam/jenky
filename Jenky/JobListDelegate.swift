545a5920e028ecd19c49c52e9bfb339566aa0a7f
Colin Clark <cclark@ccs.neu.edu>
2015-05-07 17:03:23 -0400

    Moved job configuration to user defaults instead of hard coding it

--- Jenky/JobListDelegate.swift
+++ Jenky/JobListDelegate.swift
@@ -10,14 +10,11 @@ import Cocoa
 import Foundation
 
 class JobListDelegate: NSObject, NSTableViewDelegate, NSTableViewDataSource {
-    let jenkinsURL = NSURL(string: "http://some.jenkins.server")
-
-    let jobs:[(String, String)] = [
-        ("Job Name A", "jobPathA"),
-        ("Job Name B", "jobPathB")]
+    var jobs: NSArray?
+    var jenkinsURL: NSURL?
        
     func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
-        return jobs.count
+        return getOrInitJobs().count
     }
     
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -27,11 +24,46 @@ class JobListDelegate: NSObject, NSTableViewDelegate, NSTableViewDataSource {
         
         println("Getting view!")
 
-        let jobURL = NSURL(
-            string: "job/" + jobs[row].1 + "/",
-            relativeToURL: jenkinsURL)
+        let jobDic = getOrInitJobs()[row] as! NSDictionary
+        let jobId = jobDic.valueForKey("id") as! String
+        let jobName = jobDic.valueForKey("name") as! String
 
-        cellView.setJob(JenkinsJob(url: jobURL!), name: jobs[row].0)
+        let jobURL = NSURL(string: "job/" + jobId + "/",
+            relativeToURL: getOrInitServer())
+
+        cellView.setJob(JenkinsJob(url: jobURL!), name: jobName)
         return cellView
+    }
+
+    private func getOrInitJobs() -> NSArray {
+        if let existingJobs = jobs {
+            return existingJobs
+        } else {
+            let userJobs = NSUserDefaults.standardUserDefaults()
+                .arrayForKey("jobs")
+
+            if userJobs != nil {
+                jobs = userJobs
+            } else {
+                jobs = NSArray()
+            }
+            return jobs!
+        }
+    }
+
+    private func getOrInitServer() -> NSURL {
+        if let existingURL = jenkinsURL {
+            return existingURL
+        } else {
+            let userURL = NSUserDefaults.standardUserDefaults()
+                .stringForKey("jenkinsServer")
+
+            if userURL != nil {
+                jenkinsURL = NSURL(string: userURL!)
+            } else {
+                jenkinsURL = NSURL()
+            }
+            return jenkinsURL!
+        }
     }
 }
